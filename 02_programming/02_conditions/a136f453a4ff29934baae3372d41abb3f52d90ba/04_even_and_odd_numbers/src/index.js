2385851b0ee19a4ee258adbf0a390d5edddba57b
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-12 12:01:00 +0100

    finish even & odd numbers exercices

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/04_even_and_odd_numbers/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/04_even_and_odd_numbers/src/index.js
@@ -2,3 +2,9 @@ let numberToCheck = 18;
 
 // Code here a conditional statement that will log "The number is even" when
 // numberToCheck is even and "The number is odd" when it is odd
+if(numberToCheck % 2){
+  console.log("The number is odd");
+}
+else{
+  console.log("The number is even");
+}
