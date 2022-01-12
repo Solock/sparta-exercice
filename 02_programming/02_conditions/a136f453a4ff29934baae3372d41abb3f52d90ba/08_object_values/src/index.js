c185fada46d6331444685d5330761c8698f066be
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-12 14:43:31 +0100

    finish objcet values exercices

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
@@ -11,14 +11,19 @@ const odinRequest = "Mjöllnir";
 
 // Code here a constant armouryWithNamesOnly bound to an array called that has all the values of the object armoury
 // using the object method to get all values (google is your friend)
-
+const armouryWithNamesOnly = [armoury.spear, armoury.hammer, armoury.chain, armoury.sword, armoury.shoes, armoury.horn];
 
 
 // Create a constant isTheRequestInArmoury bound to the check of the presence
 // of odinRequest in armouryWithNamesOnly
-
+const isTheRequestInArmoury = armouryWithNamesOnly.includes(odinRequest);
 
 
 // Create a condition that logs "Ah, I finally found something to hammer that nail into"
 // if the isTheRequestInArmoury is true and "What do you want me to do with this?" if not
-
+if(isTheRequestInArmoury !== true){
+  console.log("What do you want me to do with this?");
+}
+else{
+  console.log("Ah, I finally found something to hammer that nail into");
+}
