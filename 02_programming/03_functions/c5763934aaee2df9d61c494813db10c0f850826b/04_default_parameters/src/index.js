ad58d2ef7e4c4a07477adea173b5470d0090e18f
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-13 16:34:44 +0100

    Finish default parameter exercice

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
@@ -1,6 +1,11 @@
-function helloSpartan() {
+function helloSpartan(name = "soldier") {
   // Code the function here.
+  if(name !== "soldier"){
+    console.log(`Hello ${name}: glad to know your name!`);
+  }
+  else{
+    console.log("Hello soldier; not very talkative uh?");
+  }
 }
-
 // Do not remove last lines, it is for tests
 module.exports = helloSpartan;
