413cbdbb9398045efc9b9294391d7a4575f3ea50
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-13 12:29:33 +0100

    finish exercice fortune

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -1,5 +1,6 @@
-function tellFortune() {
+function tellFortune(jobs, city, names, years) {
   // Code the function here.
+  console.log(`You will be ${jobs} in ${city}, and married to ${names} in ${years} years.`);
 }
 
 // Do not remove last lines, it is for tests
