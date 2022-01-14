22855db43132802f0cd24e4b328e04a4f14b4adf
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-14 10:35:53 +0100

    Continue exercice

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,13 +1,40 @@
-function filteredRoles(persons, role){
+const personneArray = [
+  {
+    name: "name1",
+    role: "Teacher",
+  },
+  {
+    name: "name2",
+    role: "Student",
+  },
+  {
+    name: "name3",
+    role: "Student",
+  },
+  {
+    name: "name4",
+    role: "Student",
+  },
+];
+
+function filteredRoles(personneArray, role = "Teacher"){
+  let newArray = [];
   // Code the function here.
-  if(role === "Teacher"){
-    console.log(`${persons}, ${role}`);
+  //console.log((personneArray.filter(element => element.role === "Student")).lenght);
+  if((personneArray.filter(element => element.role === role)).lenght === 0){
+    console.log("toto");
   }
   else{
-    return filteredRoles;
+    if(role !== "Teacher"){
+      newArray = personneArray.filter(element => element.role === "Student");
+      return newArray;
+    }
+    else{
+      newArray = personneArray.filter(element => element.role === "Teacher");
+      return newArray;
+    }
   }
-
 }
-
+filteredRoles(personneArray, "toto");
 // Do not remove lines below, it is for tests
 module.exports = filteredRoles;
