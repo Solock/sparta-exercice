70ae7081dfcf56ee6e4bef2e7cb0a97b38d3d252
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-17 10:45:32 +0100

    Finish 01 exercices

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
@@ -1,7 +1,11 @@
-function greeter(people) {
-  // Code the function here.
+const people =  [ "Fenn", "Martin"];
 
+
+function greeter(people) {
+  people.forEach(element => console.log(`Hello ${element}!`));
 }
+//Code the function here
+
 
 // Do not remove the following lines, it is for tests
 module.exports = greeter;
