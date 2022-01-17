a00412b532996656281244dcf31ea270684b1267
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-17 16:06:50 +0100

    Finish exercices 2

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,22 +1,26 @@
 function addition(valeur1, valeur2, signe) {
+  return addition(valeur1 + valeur2);
   // Code here
 }
 
 function substraction(valeur1, valeur2, signe) {
+  return substraction(valeur1 - valeur2);
   // Code here
 }
 
 function multiplication(valeur1, valeur2, signe) {
+  return multiplication(valeur1 * valeur2);
   // Code here
 }
 
 function division(valeur1, valeur2, signe) {
+  return division.callback(valeur1 / valeur2);
   // Code here
 }
 
-function chooseOperation(valeur1, valeur2, signe) {
+function chooseOperation(valeur1, valeur2, callback) {
   if(Number.isInteger(valeur1 / valeur2)){
-    console.log(chooseOperation);
+    const result = callback.chooseOperation;
   }
   else{
     throw new Error("It's not integer numbers, try again.");
--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
@@ -1,6 +1,20 @@
 function theEFilter(sentences) {
 
+  const newSentence = [];
+
+  sentences.forEach(element => {
+    if(element.includes("e") || element.includes("E")){
+      newSentence.push(element);
+    }
+  });
+  return newSentence;
 }
+
+// const sentences = ["Mario", "Luigi", "PEach", "Toad", "Bowser"];
+
+// const resultat = theEFilter(sentences);
+
+// console.log(resultat);
 
 // Do not remove the following line, it is for tests
 module.exports = theEFilter;
