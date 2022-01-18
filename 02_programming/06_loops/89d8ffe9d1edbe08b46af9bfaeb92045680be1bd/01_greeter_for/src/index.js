694980ea6664e6d05ba6d7b3af58d55b0e380220
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-18 09:59:23 +0100

    Finish exercice 1

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/01_greeter_for/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/01_greeter_for/src/index.js
@@ -1,6 +1,8 @@
 function greeter(people) {
+  for (const name of people) {
+    console.log(`Hello ${name}!`);
+  }
   // Code the function here.
-
 }
 
 // Do not remove the following lines, it is for tests
