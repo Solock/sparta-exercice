729bbbfbac41b17afd9da61f5889d8201fb07d17
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-18 09:02:47 +0100

    Continue exercice 3

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
@@ -1,6 +1,19 @@
 function canTakeArtPieces(pieces) {
 
+  if(Object.keys(pieces).
+    filter((type) => type.includes("painting") !== true )){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
+
+const art = {piece: "Mona Lisa", type: "painting"};
+
+const resultat = canTakeArtPieces(art);
+
+
 
 // Do not remove the following line, it is for tests
 module.exports = canTakeArtPieces;
