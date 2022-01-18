6ccb7eda8c771834ab887af9c80b56bf12daaf6d
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-18 15:57:22 +0100

    Finish exercice 3

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
@@ -1,7 +1,24 @@
-function mapLoop() {
-  // Code the function here.
+function mapLoop(someNumbers, double) {
 
+  let newNumbers = [];
+
+  for (const element of someNumbers){
+    const result = double(element);
+
+    newNumbers.push(result);
+  }
+  // console.log({newNumbers});
+  return newNumbers;
+  // Code the function here.
 }
+
+// const someNumbers = [1, 2, 3, 4];
+
+// function double(someNumbers) {
+//   return someNumbers * 2;
+// }
+
+// mapLoop(someNumbers, double);
 
 // Do not remove the following lines, it is for tests
 module.exports = mapLoop;
