f469e9a0664ef53a4ceeca677e8c6d9a03cf3d88
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-19 09:56:12 +0100

    Finish exercice 4

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
@@ -1,6 +1,37 @@
 function sing () {
+
+  const songLyrics = [];
+
+  for (let i = 99; i >= 0; i--) {
+
+    if (i === 2) {
+      songLyrics.push("2 bottles of beer on the wall, 2 bottles of beer.");
+      songLyrics.push("Take one down and pass it around, 1 bottle of beer on the wall.");
+    }
+    else if (i === 1) {
+      songLyrics.push("1 bottle of beer on the wall, 1 bottle of beer.");
+      songLyrics.push("Take one down and pass it around, no more bottles of beer on the wall.");
+    }
+    else if (i === 0) {
+      songLyrics.push("No more bottles of beer on the wall, no more bottles of beer.");
+      songLyrics.push("Go to the store and buy some more, 99 bottles of beer on the wall.");
+
+    }
+    else{
+      songLyrics.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
+      songLyrics.push(`Take one down and pass it around, ${i - 1} bottles of beer on the wall.`);
+    }
+  }
+
+  return songLyrics;
+
   // code here
-};
+}
+
+const song = sing();
+console.log(song);
+
+
 
 // Do not remove the following line, it is for tests
 module.exports = sing;
