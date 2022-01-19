98cf5b58f3beac6a6905010865dfa8ca71fac605
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-19 10:51:41 +0100

    Finish exercice 2

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -1,4 +1,19 @@
 const identityForm = (reader) => {
+
+  reader.question("What is your first name? ", (first) =>{
+
+
+    reader.question("What is your last name? ", (last) =>{
+
+
+      reader.question("age", (age) =>{
+
+        console.log(`Your name is ${first} ${last} and you are ${age}.`);
+
+        reader.close();
+      });
+    });
+  });
   // Code here
 };
 
