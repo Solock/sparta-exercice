a4c9c3b1127491910699865d6c59a66463da83bb
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-20 09:06:35 +0100

    Continue exercice 3

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
@@ -1,5 +1,40 @@
 function numberGame(reader, min = 1, max = 100) {
   // code here
+
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  const mystere = getRandomArbitrary(1, 100);
+  console.log(mystere);
+
+  console.log("Bienvenue dans notre jeu!");
+  console.log("Trouvez le nombre mystere compris entre 1 et 100!");
+  console.log("Bonne Chance!");
+
+  function condition(number){
+    if (mystere > number) {
+      console.log("Too low");
+    } else if (mystere < number) {
+      console.log("Too hight");
+    }
+    else if(mystere === number){
+      console.log("You won!");
+    }
+    else{
+      console.log("It's not a number!");
+    }
+  }
+
+  function
+
+  reader.question("Entrez un nombre svp ", (number) =>{
+    condition(number);
+    do {
+      condition(number)
+    } while (condition(number) !== mystere);
+    reader.close();
+  }
 }
 
 module.exports = numberGame;
