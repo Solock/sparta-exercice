d57e55bbf010039cf0d22d9aa2d213170c2549b7
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-25 09:42:23 +0100

    Clean repo

---
+++ 02_programming/10_modules/fed8e7548d1850dcfb910812815428243660799b/03_play_with_time/src/playWithTime.js
@@ -0,0 +1,32 @@
+import { moment } from "moment";
+
+
+function formatDate (day){
+  let stringDate = moment(day).format("MMM Do YY") ;
+  console.log(stringDate);
+}
+
+function yearsSinceDate (){
+
+}
+
+function daysSinceDate (){
+
+}
+
+function getDayFromDate (){
+
+}
+
+export { formatDate, yearsSinceDate, getDayFromDate, daysSinceDate };
+
+
+
+
+
+
+
+
+
+
+
