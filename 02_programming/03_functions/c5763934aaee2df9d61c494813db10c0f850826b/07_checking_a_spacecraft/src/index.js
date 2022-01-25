01b539f4fe53f74f99937ec2a9b1c89126414df4
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-25 14:45:13 +0100

    Creat two arrow function for check fuel & check cargo hold with ternary condition

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,5 +1,23 @@
 // Code here
 
+const stealthyFuelLevelCheck = (fuel) => (fuel < 10) ? "Fuel level: red" : (fuel < 20 && fuel >= 10) ? "Fuel level: yellow" : "Fuel level: green";
+
+const stealthyCargoHoldCheck = (item) => {
+
+  let load = item.length;
+
+  if (load === 4 ) {
+    return "Full";
+
+  } else if (load  > 0 && load < 4) {
+    return "Spaces available: " + (4 - load);
+
+  } else if (load  > 4) {
+    return "Over capacity by " + (load - 4) + " items.";
+  }
+};
+
+
 // Do not remove last lines, it is for tests
 
 module.exports = { stealthyFuelLevelCheck, stealthyCargoHoldCheck };
