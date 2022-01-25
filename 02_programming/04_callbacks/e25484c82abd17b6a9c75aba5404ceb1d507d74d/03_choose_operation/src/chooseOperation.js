684b898ea0a631ee8df74ed32bc04a7a9e0783e0
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-25 16:56:29 +0100

    Creat 4 aritmetique functions and crcreat an chooseOperation function with use callback, finish exercices 3

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,26 +1,26 @@
-function addition(valeur1, valeur2, signe) {
-  return addition(valeur1 + valeur2);
+function addition(valeur1, valeur2) {
+  return valeur1 + valeur2;
   // Code here
 }
 
-function substraction(valeur1, valeur2, signe) {
-  return substraction(valeur1 - valeur2);
+function substraction(valeur1, valeur2) {
+  return valeur1 - valeur2;
   // Code here
 }
 
-function multiplication(valeur1, valeur2, signe) {
-  return multiplication(valeur1 * valeur2);
+function multiplication(valeur1, valeur2) {
+  return valeur1 * valeur2;
   // Code here
 }
 
-function division(valeur1, valeur2, signe) {
-  return division.callback(valeur1 / valeur2);
+function division(valeur1, valeur2) {
+  return valeur1 / valeur2;
   // Code here
 }
 
 function chooseOperation(valeur1, valeur2, callback) {
-  if(Number.isInteger(valeur1 / valeur2)){
-    const result = callback.chooseOperation;
+  if (Number.isInteger(valeur1) && Number.isInteger(valeur2) && callback === addition || callback === substraction || callback === multiplication || callback === division) {
+    return callback(valeur1, valeur2);
   }
   else{
     throw new Error("It's not integer numbers, try again.");
