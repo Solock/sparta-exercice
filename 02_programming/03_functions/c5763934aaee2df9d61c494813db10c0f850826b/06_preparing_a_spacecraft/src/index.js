cbab152236fe961d616c700b1cf94b3246af41f4
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-25 14:24:08 +0100

    Finish Function exercice n6

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
@@ -1,10 +1,38 @@
-function checkFuelLevel() {
-  // Code here
+function checkFuelLevel(fuel) {
+
+  const fullFuelLevel = "Fuel level: green";
+
+  const mediumFuelLevel = "Fuel level: yellow";
+
+  const lowFuelLevel = "Fuel level: red";
+
+  if (fuel < 10) {
+    return lowFuelLevel;
+  }
+  else if (fuel < 20 && fuel >= 10) {
+    return mediumFuelLevel;
+  }
+  else {
+    return fullFuelLevel;
+  }
 }
 
-function checkCargoHold(){
-  // Code here
+function checkCargoHold(item){
+
+  let load = item.length;
+
+  if (load === 4 ) {
+    return "Full";
+
+  } else if (load  > 0 && load < 4) {
+    return "Spaces available: " + (4 - load);
+
+  } else if (load  > 4) {
+    return "Over capacity by " + (load - 4) + " items.";
+  }
 }
+
+console.log(checkCargoHold(2));
 
 // Do not remove last lines, it is for tests
 module.exports = { checkFuelLevel, checkCargoHold };
