69e4ec8472505a8470e9cb63229c59732f819511
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-26 15:58:15 +0100

    Create conditions who check if parameters are good values & check when character egal to element into array & a error message

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
@@ -1,6 +1,33 @@
-const findPosition = () => {
-  // Code here
+const findPosition = (array, character, index ) => {
+
+
+  //let found = array.find(element => element === character);
+
+  if (typeof array === "object" && typeof array[index] == "string" && typeof character === "string" && typeof index === "number") {
+
+    if (array[index] !== character) {
+      if (index + 1 < array.length) {
+        return findPosition(array, character, index + 1);
+      }
+      else{
+        return `${character} is not present in this array.`;
+      }
+    }
+    else if( array[index] === character){
+
+      return `${character} is at position #${index + 1} in this array.`;
+
+    }
+  }
+  else {
+
+    throw new Error("Parameter needed.");
+
+  }
+
 };
+  // Code here
+
 
 // Leave line below for tests to work properly
 module.exports = findPosition;
