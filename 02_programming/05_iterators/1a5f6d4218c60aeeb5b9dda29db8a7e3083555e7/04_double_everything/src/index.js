ea45345c03736de6f254ee44e959b4cccfd66804
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-27 09:46:18 +0100

    Continue exercices iterator

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/04_double_everything/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/04_double_everything/src/index.js
@@ -1,6 +1,15 @@
 function double(numbers) {
 
+
+  let doubleNumbers = numbers.map(numbers * 2);
+
+  return doubleNumbers;
+
 }
+
+
+
+
 
 // Do not remove the following line, it is for tests
 module.exports = double;
