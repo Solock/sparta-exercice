657dc37aa34c8c9e7db7de3f57119979515f271e
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-27 15:42:56 +0100

    Repeat exercice 2

--- 02_programming/06_loops/e2ab56b2e3999581251383363ad2bf087cdea05a/02_for_each_with_a_loop/src/index.js
+++ 02_programming/06_loops/e2ab56b2e3999581251383363ad2bf087cdea05a/02_for_each_with_a_loop/src/index.js
@@ -1,7 +1,14 @@
-function forEachLoop() {
+function forEachLoop(someNumbers, repeat) {
   // Code the function here.
-
+  for (const numbers of someNumbers) {
+    repeat(numbers);
+  }
 }
+
+
+
+
+const poudlard = ["Serpentard", "Griffondor", "Poufsouffle", "Serdaigle"];
 
 // Do not remove the following lines, it is for tests
 module.exports = forEachLoop;
