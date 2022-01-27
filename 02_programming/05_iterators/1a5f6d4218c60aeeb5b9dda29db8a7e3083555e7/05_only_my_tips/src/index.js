040dbd011ec498e3ba313a06ef79ab18032c49a2
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-27 15:21:47 +0100

    Use .filter/.map for filter an array with object and select the right key we want to manipulate

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
@@ -1,5 +1,27 @@
 function onlyMyTips(invoices) {
+
+  const filtered = invoices.filter((object) => object.waiter === "Yourself");
+
+  const mcQueen = filtered.map(element => element.tip);
+
+  // const guido = mcQueen.reduce(sum);
+
+  return mcQueen;
+
+  //console.log(guido);
+
+  //console.log(filtered);
+
+  // console.log("filter", typeof filtered);
+
+  // console.log("map", typeof mcQueen);
+
+  // console.log("reduce", typeof guido);
   // Code here
+}
+
+function sum(a, b) {
+  return  a + b;
 }
 
 const todaysInvoices = [
@@ -7,6 +29,9 @@ const todaysInvoices = [
   { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
   { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
 ];
+
+onlyMyTips(todaysInvoices);
+
 
 // Do not remove the following line, it is for tests
 module.exports = onlyMyTips;
