0a5f81755601c280475581874ce744e045ac2daa
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-31 10:16:11 +0100

    Clean my git branch

--- 02_programming/06_loops/e2ab56b2e3999581251383363ad2bf087cdea05a/06_draw_me_a_triangle/src/index.js
+++ 02_programming/06_loops/e2ab56b2e3999581251383363ad2bf087cdea05a/06_draw_me_a_triangle/src/index.js
@@ -1,6 +1,23 @@
-function triangle() {
+function triangle(sizeTriangle) {
+
+  let star = "";
+
+  for (let index = 1; index <= sizeTriangle; index++) {
+    for (let jindex = 1; jindex < sizeTriangle - index; jindex++) {
+      star += " ";
+    }
+
+    for (let pindex = 0; pindex < 2 * index - 1; pindex++) {
+      star += "*";
+    }
+    star += "\n";
+  }
+  console.log(star);
   // Code here
+
 }
+
+triangle(8);
 
 // Do not remove the following line, it is for tests
 module.exports = triangle;
