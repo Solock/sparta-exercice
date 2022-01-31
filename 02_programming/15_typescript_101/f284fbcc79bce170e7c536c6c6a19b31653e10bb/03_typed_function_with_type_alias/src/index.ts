9ca5e7d9fffa812f087e7e8c9f6f65d728b43361
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-01-31 14:59:03 +0100

    ???

--- 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
@@ -1,6 +1,22 @@
-function handleTuple() {
+
+type MyTuple = [string, number];
+
+function handleTuple(value: MyTuple): void {
+
+  if (typeof value[0] === "string") {
+    console.log(`${value[0]} is a string`);
+  }
+
+  if (typeof value[1] === "number") {
+    console.log(`${value[1]} is a number`);
+  }
+
   // Code the function here.
 }
+
+const poudlard: MyTuple = ["Harry", 12];
+
+handleTuple(poudlard);
 
 // Leave the line below for tests to work properly.
 export { handleTuple };
