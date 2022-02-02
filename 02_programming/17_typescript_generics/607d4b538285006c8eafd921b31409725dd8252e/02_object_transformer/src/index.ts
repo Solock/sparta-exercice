816419bdea8d23dfc8541e9df4ec629a27378ea6
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-02 15:27:57 +0100

    Finish exercice 2

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
@@ -1,3 +1,11 @@
+type Product =  {
+  productName: string;
+  quantity: number;
+}
+
+function productTransformer(objet: Product): [string, string | number][] {
+  return [["productName", objet.productName], ["quantity", objet.quantity]];
+}
 
 // Leave the line below for tests to work properly.
 export { productTransformer };
