4179c906b46e0a6a59f8bb8876f8ddb23f2fc27b
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-02 16:38:04 +0100

    Finish exercice 3

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
@@ -1,4 +1,27 @@
 
+type Human = {
+  name: string;
+  age: number;
+  knowsDev: boolean;
+}
+
+
+type Dev = Human & {knownLanguage: string};
+
+
+function becomeADev(objet: Human): Dev {
+  const mcQueen: Dev = {
+    name: objet.name,
+    age: objet.age,
+    knowsDev: true,
+    knownLanguage: "JavaScript",
+  }
+  return mcQueen;
+}
+
+
+
+
 
 // Leave the line below for tests to work properly.
 export { becomeADev };
