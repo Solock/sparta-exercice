f06727d1b061cd4518668da4d73b5b39d989ce34
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-03 14:42:31 +0100

    Finish exercices 1 with multiple tests

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/01_greeting/src/index.test.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/01_greeting/src/index.test.ts
@@ -1,7 +1,29 @@
 import { greet } from "./index";
 
-test("Enter a string equal name in parameter and result be 'Hello string' but string is in UpperCase", function() {
-  const result = greet("McQueen");
+// test("Enter a string equal name in parameter and result be 'Hello string' but string is in UpperCase", function() {
+describe("Try some tests", () =>{
 
-  expect(result).toBe("Hello MCQUEEN!")
+  test("Test with value in name", function(){
+    const result = greet("Francis");
+    const result1 = greet("Bob");
+    const result2 = greet("Sofiane");
+
+    expect(result).toBe("Hello FRANCIS!");
+    expect(result1).toBe("Hello BOB!");
+    expect(result2).toBe("Hello SOFIANE!");
+  })
+
+  test("test without value in name", function(){
+    const result = greet();
+    expect(result).toBe("Hello WORLD!");
+  })
+
+  test("test value is string", function() {
+    const result = greet("Pablo");
+    const result1 = greet("");
+
+    expect(typeof result).toBe("string");
+    expect(typeof result1).toBe("string");
+  })
 })
+// })
