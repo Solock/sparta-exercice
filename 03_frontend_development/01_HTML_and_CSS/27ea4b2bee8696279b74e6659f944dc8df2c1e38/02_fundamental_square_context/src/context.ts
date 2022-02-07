5185694865a6845580de052923dedc809d804653
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-07 10:17:23 +0100

    Finish Warmup 02

--- 03_frontend_development/01_HTML_and_CSS/27ea4b2bee8696279b74e6659f944dc8df2c1e38/02_fundamental_square_context/src/context.ts
+++ 03_frontend_development/01_HTML_and_CSS/27ea4b2bee8696279b74e6659f944dc8df2c1e38/02_fundamental_square_context/src/context.ts
@@ -1,5 +1,10 @@
 import {squareNumber} from "./squareNumber"
 
 export function context(arrayOfNumbers: number[]): number[] {
+  let newArray: number[] = [];
+
+  newArray = arrayOfNumbers.map(element => squareNumber(element))
+
+  return newArray;
   // Code the function here
 }
--- 03_frontend_development/01_HTML_and_CSS/27ea4b2bee8696279b74e6659f944dc8df2c1e38/02_fundamental_square_context/src/squareNumber.ts
+++ 03_frontend_development/01_HTML_and_CSS/27ea4b2bee8696279b74e6659f944dc8df2c1e38/02_fundamental_square_context/src/squareNumber.ts
@@ -1,5 +1,7 @@
 // Copy and paste your function `squareNumber` here from exercise one
 
 export function squareNumber(number: number): number {
-
+  let square:number = 0;
+  square = number * number;
+  return square;
 }
