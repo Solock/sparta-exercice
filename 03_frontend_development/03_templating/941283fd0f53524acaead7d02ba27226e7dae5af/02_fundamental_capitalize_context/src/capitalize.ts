b675cbe1f8c08baaa9d7f5bb077217bdeba7e4a4
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-09 11:28:51 +0100

    Finish exercices 2

--- 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/capitalize.ts
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/capitalize.ts
@@ -1,5 +1,7 @@
 // Copy your function `capitalize` from the first exercise
 
 export function capitalize(word: string): string {
+  const capitalizer: string = word.charAt(0).toUpperCase()+word.slice(1);
+  return capitalizer;
   // Code your function here
 }
--- 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
@@ -1,5 +1,13 @@
 import { capitalize } from "./capitalize"
 
 export function context(sentence: string): string {
+  const phraseArray: string[] = sentence.split(" ");
+  //console.log(phraseArray);
+
+  const stock: string [] =  phraseArray.map(capitalize);
+  const phrase: string = stock.join(" ");
+
+  return phrase;
   // Code here and use the function `capitalize` from exercise one
 }
+
