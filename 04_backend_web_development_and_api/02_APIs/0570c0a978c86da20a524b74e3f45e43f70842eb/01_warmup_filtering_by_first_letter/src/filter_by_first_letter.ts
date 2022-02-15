228b6577aa1e0e23efc4be61981709324283c0ea
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-15 10:42:45 +0100

    Finish exercice 2

--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
@@ -1,6 +1,7 @@
 import { Customer } from "../data/customers";
 
 export function filterByFirstLetter(listCustomer: Customer[], letter: string): Customer[] {
-  return listCustomer.filter((element) => element.last_name.charAt(0) === letter);
+  const result = listCustomer.filter((element) => element.last_name.charAt(0) === letter);
+  return result;
   // Code here
 }
--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
@@ -1,1 +1,19 @@
 // code here
+import { Customer } from "../data/customers";
+
+export function filterByFirstLetterOrdered(listCustomer: Customer[], letter: string): Customer[] {
+  const result = listCustomer.filter((element) => element.last_name.charAt(0) === letter);
+
+  function comparse( result1: Customer, result2: Customer) {
+    if (result1.last_name < result2.last_name) {
+      return -1;
+    }
+    if (result1.last_name > result2.last_name) {
+      return 1;
+    }
+    return 0
+  }
+  return result.sort(comparse);
+  // Code here
+}
+
