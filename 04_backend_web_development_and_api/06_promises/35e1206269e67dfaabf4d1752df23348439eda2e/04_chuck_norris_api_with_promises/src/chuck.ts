ce288d734f253d50c0ada1710565efb0e62b839c
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-21 15:47:10 +0100

    Finish exercice 04

---
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/04_chuck_norris_api_with_promises/src/chuck.ts
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+
+function getCategories(): Promise<void> {
+  return fetch("https://api.chucknorris.io/jokes/categories").then((response) => response.json());
+  // Your code goes here
+}
+
+function getJoke(category: string): Promise<any> {
+  return fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
+    .then((response) => response.json())
+    .then((joke) => joke.value);
+  // Your code goes here
+}
+
+// Leave the line below for tests to work properly
+export { getCategories, getJoke };
