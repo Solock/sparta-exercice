fc8362a53ee5810c97a1bdd58e6a13490fd19118
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-23 14:08:39 +0100

    Finish exercice 03

--- 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/03_warmup_rectangle/src/rectangle.ts
+++ 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/03_warmup_rectangle/src/rectangle.ts
@@ -1,2 +1,13 @@
 export function rectangle(width: number, height: number): string {
+  let space = "";
+  for (let index = 0; index < height; index++) {
+    for (let jindex = 0; jindex < width; jindex++) {
+      if (jindex === width - 1) {
+        space += "*\n";
+      } else {
+        space += "*";
+      }
+    }
+  }
+  return space;
 }
