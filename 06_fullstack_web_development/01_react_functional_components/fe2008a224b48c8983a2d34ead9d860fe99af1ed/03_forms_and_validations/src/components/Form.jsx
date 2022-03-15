40b77a607b3ff52880da8c5e2766ebfa97651bd7
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-03-15 14:59:19 +0100

    Finish exercice yesterday

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [error, setError] = useState(false);
+
+  function verifInput(event) {
+    const evenement = event.target.value;
+
+    if (evenement.includes(" ")) {
+      setError(<p>no spaces allowed</p>);
+      return true;
+    } else {
+      setError(false);
+      return false;
+    }
+  }
+  function verifSubmit() {
+    console.log(document.getElementById("check").value);
+  }
+  return (
+    <div>
+      <form onSubmit={verifSubmit}>
+        <label>First Name</label>
+        <input id="check" onChange={verifInput} type="text" name="FirstName" />
+        <button type="submit">Submit</button>
+      </form>
+      {error}
+    </div>
+  );
+};
 
 export default Form;
